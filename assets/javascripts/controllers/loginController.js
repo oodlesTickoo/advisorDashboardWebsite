d5d5e7e9d8a6ac1e80f3ef7683a74d1a0fb6bc3a
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2017-05-26 11:07:40 +0530

    login integration

--- assets/javascripts/controllers/loginController.js
+++ assets/javascripts/controllers/loginController.js
@@ -1,3 +1,32 @@
-app.controller("LoginController", ['$scope', function($scope) {
+app.controller("LoginController", ['$scope', 'AuthenticationService', function($scope, AuthenticationService) {
+         'use strict';
 
-}]);
+         $scope.user={};
+
+         $scope.login=function(type) {
+             //vm.dataLoading = true;
+          	console.log("user: ",$scope.user);
+             $scope.user.type=type;
+             AuthenticationService.Login($scope.user, function(response) {
+             	console.log('response: ',response);
+                 if (response.success) {
+                     
+                 } else {
+    
+                     //vm.dataLoading = false;
+                 }
+             });
+         };
+
+     }]);
+
+
+/*function SaveToDisk(fileURL, fileName) {
+        var link = document.createElement('a');
+        document.body.appendChild(link);
+        link.href = fileURL;
+        link.target='_blank';
+        link.click();
+        $rootScope.isLoading = false;
+        $timeout(0);
+    }*/
