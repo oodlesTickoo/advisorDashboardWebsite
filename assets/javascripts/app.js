21440d24c80a5a1cd987950705711b49086c6e8b
Milind Ahuja <milind.ahuja@oodlestechnologies.com>
2017-06-02 18:24:06 +0530

    resolved scrollbar issue on calculator page

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -28,7 +28,7 @@ app.config(function($stateProvider, $urlRouterProvider, ScrollBarsProvider, $loc
         scrollInertia: 800,
         axis: 'y', // enable 2 axis scrollbars by default 
         theme: 'light',
-        setHeight: 620,
+        setHeight: 635,
         autoHideScrollbar: true
     };
 
