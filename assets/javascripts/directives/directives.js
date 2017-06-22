00a1050c610bdd1186e91e6901358498750e6625
Milind Ahuja <milind.ahuja@oodlestechnologies.com>
2017-06-22 16:51:58 +0530

    added button to see calculator result

--- assets/javascripts/directives/directives.js
+++ assets/javascripts/directives/directives.js
@@ -27,20 +27,4 @@ app.directive("fileread", ['$http', function($http) {
             });
         }
     }
-}])
-
-.directive("customPopover", [ function() {
-    return {
-        restrict: 'A',
-        template: '<span>{{label}}</span>',
-        link: function (scope, el, attrs) {
-            scope.label = attrs.popoverLabel;
-            $(el).popover({
-                trigger: 'hover',
-                html: true,
-                content: attrs.popoverHtml,
-                placement: attrs.popoverPlacement
-            });
-        }
-    };
 }]);
